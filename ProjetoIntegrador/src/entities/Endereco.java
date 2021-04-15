f2a8b7b7b0f7a6316a030181c3f99fd7b69ff9c8
audrynwo <82619439+audrynwo@users.noreply.github.com>
2021-04-15 20:40:46 -0300

    Add files via upload

---
+++ ProjetoIntegrador/src/entities/Endereco.java
@@ -0,0 +1,74 @@
+package entities;
+
+/**
+ * Classe responsável por representar o Endereço do Usuário
+ * @author Audryn Weber de Oliveira
+ * */
+public class Endereco {
+	private String uf;
+	private String cidade;
+	private String bairro;
+	private String rua;
+	private String complemento;
+	private String cep;
+	
+	public Endereco(String cep, String uf, String cidade, String bairro, String rua, String complemento) {
+		this.cep = cep;
+		this.uf = uf;
+		this.cidade = cidade;
+		this.bairro = bairro;
+		this.rua = rua;
+		this.complemento = complemento;
+		
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+}
+	
