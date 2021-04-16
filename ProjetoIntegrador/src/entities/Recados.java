91b0995d0ceef72e3ad81041b77dd873094dbd8d
kaianesferreira <82619482+kaianesferreira@users.noreply.github.com>
2021-04-15 22:48:58 -0300

    Add files via upload
    Classe recados

---
+++ ProjetoIntegrador/src/entities/Recados.java
@@ -0,0 +1,49 @@
+package entities;
+
+/**
+ * Classe responsável por representar os recados
+ * @author Kaiane Ferreira
+ * */
+
+public class Recados {
+	
+	private String titulo;
+	private String texto;
+	private String midia;
+	
+	public Recados(String titulo, String texto, String midia) {
+		this.titulo = titulo;
+		this.texto = texto;
+		this.midia = midia;
+	}
+	
+	public Recados(String titulo, String texto) {
+		this.titulo = titulo;
+		this.texto = texto;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getMidia() {
+		return midia;
+	}
+
+	public void setMidia(String midia) {
+		this.midia = midia;
+	}
+	
+}
